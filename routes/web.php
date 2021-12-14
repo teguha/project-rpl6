ea9acb1887f1a948cfd941044bf022a4eeb91147
bot99 <teguharthana@gmail.com>
2021-12-14 14:32:47 +0800

    save to github

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\HomeController; //perintah supaya dapat menggunakan con
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/',[HomeController::class,'index']);
+Route::get('/addBanjar',[HomeController::class,'index'])->name('add-banjars');
 Route::get('/logins',[HomeController::class,'tes']);
 Route::get('/coba',function(){
     return view('coba');
@@ -39,24 +39,29 @@ Route::group(['midlleware'=>['auth']],function(){
     });
 
     Route::group(['middleware'=>['cek_login:2']],function(){
-        Route::get('Banjar_A',[UserController::class,'logins'])->name('Banjar_A-dasboard');
+        Route::get('Banjar_Digital',[AdminController::class,'index'])->name('Banjar_Digital-dasboard');
     });
 
     Route::group(['middleware'=>['cek_login:1']],function(){
-        Route::get('kr_swela',[UserController::class,'logins'])->name('kr_swela-dasboard');
+        Route::get('Banjar_A',[UserController::class,'logins'])->name('Banjar_A-dasboard');
     });
 
     Route::get('/{id}/edit-Agenda',[UserController::class,'edit_agenda'])->name('agenda-edit');
     Route::get('/{id}/edit-upacara',[UserController::class,'edit_upacara'])->name('upacara-edit');
     Route::post('/logout',[AuthController::class,'logout'])->name('user-logout');
+    Route::get('{id}/Banjarr-edit',[AdminController::class,'banjarEdit'])->name('banjar-edited');
+    Route::get('{id}/sejarah-edits',[AdminController::class,'sejarahEdit'])->name('Sejarah-editss');
+    Route::get('sejarah-data',[AdminController::class,'sejarahData'])->name('Sejarah-user');
+    Route::get('banjars-data',[AdminController::class,'banjarData'])->name('banjars-user');
+    Route::post('{id}/banjars-saves',[AdminController::class,'BanjarssEdit'])->name('saves-edit-banjar');
     
 });
 
-
+Route::POST('banjar_save',[AdminController::class,'BanjarAdd'])->name('views-banjarr-saving');
 Route::get('/banjar',[HomeController::class,'banjar'])->name('banjar-view');
 Route::post('message_save',[UserController::class,'message'])->name('message-save');
 Route::get('message_data',[UserController::class,'message_data'])->name('message-view');
-Route::get('/{id}/message-detail',[UserController::class,'message_detail'])->name('message-details');
+Route::get('/{id}/message-detail',[AdminController::class,'messageDetail'])->name('message-details');
 Route::post('/{id}/message-delete',[UserController::class,'message_delete'])->name('message-delete');
 
 Route::get('/posting',[HomeController::class,'posting'])->name('login-view');
@@ -87,10 +92,40 @@ Route::prefix('/dashboard-user')->group(function(){
 
     Route::post('/{id}/editsaveUpacara',[UserController::class,'UpacaraEditSave'])->name('upacara-edits');
     Route::post('/{id}/deleteupacara',[UserController::class,'delete_upacara'])->name('upacara-delete');
+
+
+    //owner dashboard--------------------------------//
+    Route::get('/Dashboard-owner',[AdminController::class,'dashboard'])->name('dashboard-users');
+    Route::get('/Profile-owner',[AdminController::class,'profile'])->name('profile-users');
+    
+    Route::get('/anggota-data',[AdminController::class,'anggota'])->name('Anggota-user');
+    Route::get('/anggota-detail',[AdminController::class,'anggotaDetail'])->name('anggota-detail');
+    Route::get('/anggota-edit',[AdminController::class,'anggotaEdit'])->name('anggota-edit');
+    Route::post('/anggota-delete',[AdminController::class,'Anggotadelete'])->name('anggota-delete');
+    
+
+    Route::get('sejarah-data',[AdminController::class,'sejarahData'])->name('Sejarah-user');
+    Route::get('/sejarah-add',[AdminController::class,'sejarahAdd'])->name('Sejarah-add');
+    Route::post('/sejarah-save',[AdminController::class,'sejarahSave'])->name('Sejarah-Save');
+    Route::get('/{id}/sejarah-detail',[AdminController::class,'sejarahDetail'])->name('Sejarah-detail');
+
+    Route::post('{id}/sejarah-delete',[AdminController::class,'sejarahdelete'])->name('Sejarah-delete');
+    Route::post('{id}/sejarah-edis-save',[AdminController::class,'sejarahEditSave'])->name('Sejarah-edit-saves');
+
+   
+    Route::get('/Banjar-add',[AdminController::class,'BanjarssAdd'])->name('banjar-user-add');
+    Route::get('/Banjar-detail',[AdminController::class,'BanjarssDetail'])->name('banjar-details');
+
+    Route::post('/{id}/Banjars-edit',[AdminController::class,'BanjarssEdit'])->name('banjar-edits-save');
+    Route::post('/Banjar-delete',[AdminController::class,'Banjarssdelete'])->name('banjar-deletes');
+
+    Route::get('/Message-data',[AdminController::class,'MessageData'])->name('message-user');
+    Route::get('/{id}/Message-detail',[AdminController::class,'MessageDetail'])->name('message-detail');
+    Route::post('/Message-delete',[AdminController::class,'Messagedelete'])->name('message-delete');
 });
 
 Route::get('/coba',[AdminController::class,'coba_list']);
-Route::get('{id}/banjar_info',[HomeController::class,'info'])->name('views-banjarr');
+Route::get('{id}/banjar_info',[HomeController::class,'info'])->name('info-banjar');
 Route::get('{id}/sejarah_info',[HomeController::class,'info_sej'])->name('views-sejarah');
 Route::get('/sejarah',[HomeController::class,'sejarah'])->name('add-sejarah');
 Route::post('sejarah_info',[HomeController::class,'info_sejarah'])->name('posting-sejarah');
